fa43499526af8002245513f51d47f66037ec8cb6
ico-ila <127539324+ico-ila@users.noreply.github.com>
2023-04-25 23:57:26 -0300

    Day Two Exercise 1

--- 30DaysOfJavaScript/DayTwo/exercise1.js
+++ 30DaysOfJavaScript/DayTwo/exercise1.js
@@ -1,2 +1,45 @@
-let challenge = '30DaysOfJavaScript'
+// no.1
+let challenge = '30 Days Of JavaScript'
+// no.2
 console.log(challenge)
+// no.3
+console.log(challenge.length)
+// no.4
+challenge.toUpperCase
+// no.5
+challenge.toLowerCase
+// no.6
+challenge.substring(0,2)
+// no.7
+challenge.substring(3, challenge.length)
+// no.8
+challenge.includes('Script')
+// no.9
+let array = challenge.split
+// no.10
+print(challenge.split(' '))
+// no.11
+let phrases = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+let phrasesArray = phrases.split(",")
+print(phrasesArray)
+// no.12
+challenge.replace("JavaScript", "Python")
+// no.13
+challenge.charAt(15)
+// no.14
+challenge.charCodeAt(11)
+// no.15
+challenge.indexOf("a")
+// no.16
+challenge.lastIndexOf("a")
+// no.17
+let sentence = 'You cannot end a sentence with because because because is a conjunction'
+sentence.indexOf('because')
+// no.18
+sentence.lastIndexOf('because')
+// no.19
+sentence.search("because")
+// no.20
+let word = ' 30 Days Of JavaScript '
+word.trim()
+
