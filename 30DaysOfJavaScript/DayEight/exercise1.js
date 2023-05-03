edcd5261a799cacfcc6b00344055a8556abef1e5
just-toni <75427368+just-toni@users.noreply.github.com>
2023-05-02 23:50:26 -0300

    Updated Level 1 of Day 8 Exercises

--- 30DaysOfJavaScript/DayEight/exercise1.js
+++ 30DaysOfJavaScript/DayEight/exercise1.js
@@ -5,22 +5,23 @@ const dog = {}
 console.log(dog)
 
 // no.3
-dog = {
-    name: 'Bingo',
-    legs: 4,
-    color: 'caramel brown',
-    age: 2,
-    'bark' : 'woof woof'
-}
+   dog.name = 'Bingo',
+    dog.legs = 4,
+    dog.color = 'caramel brown',
+    dog.age = 2,
+    dog.bark = 'woof woof'
 
 // no.4
-console.log(dog[bark])
+console.log(dog['bark'])
 console.log(dog.age)
 console.log(dog.name)
-console.log(dog[legs])
+console.log(dog['legs'])
 console.log(dog.color)
 
 // no.5
 dog.breed = 'German Shepherd'
-dog.getDogInfo = dog.name + " " + dog.age + " " + dog.color + " " + dog.breed + " " + dog.legs + " " + dog['bark']
+dog.getDogInfo = function(){
+    return `The name of this dog is ${dog.name} and this dog is ${dog.age} years old. It is a ${dog.breed} and has ${dog.legs} legs. It is ${dog.color} in color and likes to bark like this ${dog['bark']} `
+}
+console.log(dog.getDogInfo())
 
