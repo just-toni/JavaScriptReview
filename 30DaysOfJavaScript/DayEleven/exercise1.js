5de842c4c51d0d7ee8dd872df6273ca2a74d3778
just-toni <75427368+just-toni@users.noreply.github.com>
2023-05-07 22:04:01 -0300

    completed level 2 of day 11

--- 30DaysOfJavaScript/DayEleven/exercise1.js
+++ 30DaysOfJavaScript/DayEleven/exercise1.js
@@ -6,50 +6,6 @@ const rectangle = {
   area: 200,
   perimeter: 60
 }
-const users = [
-{
-  name:'Brook',
-  scores:75,
-  skills:['HTM', 'CSS', 'JS'],
-  age:16
-},
-{
-  name:'Alex',
-  scores:80,
-  skills:['HTM', 'CSS', 'JS'],
-  age:18
-},
-{
-  name:'David',
-  scores:75,
-  skills:['HTM', 'CSS'],
-  age:22
-},
-{
-  name:'John',
-  scores:85,
-  skills:['HTML'],
-  age:25
-},
-{
-  name:'Sara',
-  scores:95,
-  skills:['HTM', 'CSS', 'JS'],
-  age: 26
-},
-{
-  name:'Martha',
-  scores:80,
-  skills:['HTM', 'CSS', 'JS'],
-  age:18
-},
-{
-  name:'Thomas',
-  scores:90,
-  skills:['HTM', 'CSS', 'JS'],
-  age:20
-}
-]
 
 // no.1
 let [e, pi, gravity, humanBodyTemp, waterBoilingTemp] = constants;
