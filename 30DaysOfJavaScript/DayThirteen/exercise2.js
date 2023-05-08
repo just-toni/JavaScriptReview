bc740b81c7fab1cc837e82bf9b292fbc07d9fa2a
just-toni <75427368+just-toni@users.noreply.github.com>
2023-05-07 23:47:41 -0300

    completed level 2 of day 13 exercises

---
+++ 30DaysOfJavaScript/DayThirteen/exercise2.js
@@ -0,0 +1,8 @@
+// no.1
+console.assert(10 > 2 * 10, "Incorrect, 10 is less then double of 10");
+
+// no.2
+console.warn("Adding semicolons to your code makes it easier!");
+
+// no.3
+console.error("You didn't close your brackets now your code won't compile :)");
