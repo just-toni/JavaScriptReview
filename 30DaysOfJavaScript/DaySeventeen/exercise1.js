0820ff4a7700a33f1530747f4e7eaf549b0ed1a4
just-toni <75427368+just-toni@users.noreply.github.com>
2023-05-10 23:55:13 -0300

    completed level 1 of day 17 exercises

---
+++ 30DaysOfJavaScript/DaySeventeen/exercise1.js
@@ -0,0 +1,11 @@
+let firstName = 'Hyuneaq';
+let lastName = 'Mashdegdf'
+let age = 23;
+let country = 'USA';
+let city = 'Los Angeles';
+
+localStorage.setItem("firstName", firstName);
+localStorage.setItem("lastName", lastName);
+localStorage.setItem("age", age);
+localStorage.setItem("country", country);
+localStorage.setItem("city", city);
