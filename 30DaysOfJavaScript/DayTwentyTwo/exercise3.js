92c58e7c996fcdd5b4d70094bfd42ca78653e3b8
just-toni <75427368+just-toni@users.noreply.github.com>
2023-05-17 02:37:11 -0300

    almost done with level 3 for day 22, just a few edits to be made but it's good!

--- 30DaysOfJavaScript/DayTwentyTwo/exercise3.js
+++ 30DaysOfJavaScript/DayTwentyTwo/exercise3.js
@@ -6,6 +6,8 @@ let outerDiv = document.querySelector('div');
 outerDiv.appendChild(h1);
 outerDiv.style.textAlign = 'center';
 outerDiv.style.fontFamily = 'Trebuchet MS';
+outerDiv.style.padding = '0 250px';
+outerDiv.style.fontWeight = 'lighter';
 let year = document.querySelector('span');
 year.style.fontSize = '40px';
 year.style.fontWeight = 'bold'
@@ -101,14 +103,12 @@ outerDiv.appendChild(h3_1);
 outerDiv.appendChild(h3_2);
 
 let firstSection = document.createElement('section');
-firstSection.setAttribute('id', 'challenges')
+firstSection.setAttribute('id', 'challenges');
 outerDiv.appendChild(firstSection);
 let challengesUl = document.createElement('ul');
-challengesUl.setAttribute('id','challengesList')
+challengesUl.setAttribute('id','challengesList');
 firstSection.appendChild(challengesUl);
 challengesUl.style.listStyleType = 'none';
-challengesUl.style.marginLeft = '280px';
-
 
 // 30 days python
 let firstItem_1 = document.createElement('li');
@@ -120,7 +120,6 @@ challengesUl_1.style.justifyContent = 'space-between';
 challengesUl_1.style.backgroundColor = 'greenyellow';
 challengesUl_1.style.padding = '20px';
 challengesUl_1.style.margin = '5px';
-challengesUl_1.style.width = '600px';
 challengesUl_1.style.textAlign = 'left';
 challengesUl.appendChild(challengesUl_1);
 let firstItemLink = document.createElement('a');
@@ -157,7 +156,6 @@ challengesUl_2.style.justifyContent = 'space-between';
 challengesUl_2.style.backgroundColor = 'gold';
 challengesUl_2.style.padding = '20px';
 challengesUl_2.style.margin = '5px';
-challengesUl_2.style.width = '600px';
 challengesUl_2.style.textAlign = 'left';
 challengesUl.appendChild(challengesUl_2);
 let secondItemLink = document.createElement('a');
@@ -194,7 +192,6 @@ challengesUl_3.style.justifyContent = 'space-between';
 challengesUl_3.style.backgroundColor = 'coral';
 challengesUl_3.style.padding = '20px';
 challengesUl_3.style.margin = '5px';
-challengesUl_3.style.width = '600px';
 challengesUl_3.style.textAlign = 'left';
 challengesUl.appendChild(challengesUl_3);
 thirdItem_1.textContent = asabenehChallenges2020.challenges[2].name;
@@ -227,7 +224,6 @@ challengesUl_4.style.justifyContent = 'space-between';
 challengesUl_4.style.backgroundColor = 'coral';
 challengesUl_4.style.padding = '20px';
 challengesUl_4.style.margin = '5px';
-challengesUl_4.style.width = '600px';
 challengesUl_4.style.textAlign = 'left';
 challengesUl.appendChild(challengesUl_4);
 fourthItem1.textContent = asabenehChallenges2020.challenges[3].name;
@@ -260,7 +256,6 @@ challengesUl_5.style.justifyContent = 'space-between';
 challengesUl_5.style.backgroundColor = 'coral';
 challengesUl_5.style.padding = '20px';
 challengesUl_5.style.margin = '5px';
-challengesUl_5.style.width = '600px';
 challengesUl_5.style.textAlign = 'left';
 challengesUl.appendChild(challengesUl_5);
 fifthItem1.textContent = asabenehChallenges2020.challenges[4].name;
@@ -293,7 +288,6 @@ challengesUl_6.style.justifyContent = 'space-between';
 challengesUl_6.style.backgroundColor = 'coral';
 challengesUl_6.style.padding = '20px';
 challengesUl_6.style.margin = '5px';
-challengesUl_6.style.width = '600px';
 challengesUl_6.style.textAlign = 'left';
 challengesUl.appendChild(challengesUl_6);
 sixthItem1.textContent = asabenehChallenges2020.challenges[5].name;
@@ -326,7 +320,6 @@ challengesUl_7.style.justifyContent = 'space-between';
 challengesUl_7.style.backgroundColor = 'coral';
 challengesUl_7.style.padding = '20px';
 challengesUl_7.style.margin = '5px';
-challengesUl_7.style.width = '600px';
 challengesUl_7.style.textAlign = 'left';
 challengesUl.appendChild(challengesUl_7);
 seventhItem1.textContent = asabenehChallenges2020.challenges[6].name;
@@ -348,7 +341,7 @@ challengesUl_7.appendChild(seventhItem1);
 challengesUl_7.appendChild(seventhItem2);
 challengesUl_7.appendChild(seventhItem3);
 
-// 30 day react native
+// 30 day machine learning
 let eightItem1 = document.createElement('li');
 let challengesUl_8 = document.createElement('section');
 challengesUl_8.setAttribute('id', 'machinelearning');
@@ -359,7 +352,6 @@ challengesUl_8.style.justifyContent = 'space-between';
 challengesUl_8.style.backgroundColor = 'coral';
 challengesUl_8.style.padding = '20px';
 challengesUl_8.style.margin = '5px';
-challengesUl_8.style.width = '600px';
 challengesUl_8.style.textAlign = 'left';
 challengesUl.appendChild(challengesUl_8);
 eightItem1.textContent = asabenehChallenges2020.challenges[7].name;
@@ -380,3 +372,151 @@ eightItem3.textContent = asabenehChallenges2020.challenges[7].status;
 challengesUl_8.appendChild(eightItem1);
 challengesUl_8.appendChild(eightItem2);
 challengesUl_8.appendChild(eightItem3);
+
+let secondSection = document.createElement('section');
+secondSection.setAttribute('id', 'secondSection');
+outerDiv.appendChild(secondSection);
+let author = document.createElement('h2');
+author.textContent = asabenehChallenges2020.author.firstName + " " + asabenehChallenges2020.author.lastName;
+secondSection.appendChild(author); 
+author.style.fontWeight = 'lighter';
+let icons = document.createElement('ul');
+icons.style.listStyleType = 'none';
+icons.style.display = 'flex';
+icons.style.justifyContent = 'center';
+icons.style.width = '100px';
+icons.style.marginLeft = '280px';
+icons.setAttribute('id', 'iconsList');
+let iconItem1 = document.createElement('li');
+let iconItem1Link = document.createElement('a');
+iconItem1Link.setAttribute('href', asabenehChallenges2020.author.socialLinks[0].url);
+let iconItem1Image = document.createElement('img');
+let source1 = "/30DaysOfJavaScript/DayTwentyTwo/linkedInlogo.png"
+iconItem1Image.setAttribute('src', source1);
+iconItem1Image.setAttribute('alt', asabenehChallenges2020.author.socialLinks[0].social);
+iconItem1Image.style.width = '48px';
+iconItem1Image.style.marginTop = '-10px';
+iconItem1Link.appendChild(iconItem1Image);
+iconItem1.appendChild(iconItem1Link);
+icons.appendChild(iconItem1);
+
+// twitter icon
+let iconItem2 = document.createElement('li');
+let iconItem2Link = document.createElement('a');
+iconItem2Link.setAttribute('href', asabenehChallenges2020.author.socialLinks[1].url);
+let iconItem2Image = document.createElement('img');
+let source2 = "/30DaysOfJavaScript/DayTwentyTwo/twitterlogo.png"
+iconItem2Image.setAttribute('src', source2);
+iconItem2Image.setAttribute('alt', asabenehChallenges2020.author.socialLinks[1].social);
+iconItem2Image.style.width = '30px';
+// iconItem2Image.style.height = '30px';
+iconItem2.style.marginRight = '5px';
+iconItem2Link.appendChild(iconItem2Image);
+iconItem2.appendChild(iconItem2Link);
+icons.appendChild(iconItem2);
+
+// github icon
+let iconItem3 = document.createElement('li');
+let iconItem3Link = document.createElement('a');
+iconItem3Link.setAttribute('href', asabenehChallenges2020.author.socialLinks[2].url);
+let iconItem3Image = document.createElement('img');
+let source3 = "/30DaysOfJavaScript/DayTwentyTwo/githubLogo.png"
+iconItem3Image.setAttribute('src', source3);
+iconItem3Image.setAttribute('alt', asabenehChallenges2020.author.socialLinks[2].social);
+iconItem3Image.style.width = '30px';
+iconItem3Link.appendChild(iconItem3Image);
+iconItem3.appendChild(iconItem3Link);
+icons.appendChild(iconItem3);
+
+secondSection.appendChild(icons);
+
+let bio = document.createElement('p');
+bio.textContent = asabenehChallenges2020.author.bio;
+bio.style.fontWeight = 'lighter';
+bio.style.margin = '0 60px';
+secondSection.appendChild(bio);
+
+let authorsInfo = document.createElement('section');
+let titlesList = document.createElement('ul');
+let titlesTitle = document.createElement('h6');
+titlesTitle.textContent = 'Titles';
+titlesList.appendChild(titlesTitle);
+titlesTitle.style.fontWeight = 'bold';
+titlesTitle.style.fontSize = '12pt';
+titlesTitle.style.marginBottom = '10px';
+titlesList.style.listStyleType = 'none';
+titlesList.style.textAlign = 'left';
+for(let i = 0; i < asabenehChallenges2020.author.titles.length; i++){
+    const element = asabenehChallenges2020.author.titles[i];
+    let titleListItem = document.createElement('li');
+    titleListItem.textContent = element[0].concat(element[1]);
+    titlesList.appendChild(titleListItem);
+}
+authorsInfo.appendChild(titlesList);
+
+let skillsList = document.createElement('ul');
+let skillsTitle = document.createElement('h6');
+skillsTitle.textContent = 'Skills';
+skillsList.appendChild(skillsTitle);
+skillsTitle.style.fontWeight = 'bold';
+skillsTitle.style.fontSize = '12pt';
+skillsTitle.style.marginBottom = '10px';
+skillsList.style.listStyleType = 'none';
+skillsList.style.textAlign = 'left';
+for(let i = 0; i < asabenehChallenges2020.author.skills.length; i++){
+    const element = asabenehChallenges2020.author.skills[i];
+    let skillListItem = document.createElement('li');
+    skillListItem.textContent = element;
+    skillsList.appendChild(skillListItem);
+}
+authorsInfo.appendChild(skillsList);
+
+let qualificationsList = document.createElement('ul');
+let qualificationsTitle = document.createElement('h6');
+qualificationsTitle.textContent = 'Qualifications';
+qualificationsList.appendChild(qualificationsTitle);
+qualificationsTitle.style.fontWeight = 'bold';
+qualificationsTitle.style.fontSize = '12pt';
+qualificationsTitle.style.marginBottom = '10px';
+qualificationsList.style.listStyleType = 'none';
+qualificationsList.style.textAlign = 'left';
+for(let i = 0; i < asabenehChallenges2020.author.qualifications.length; i++){
+    const element = asabenehChallenges2020.author.qualifications[i];
+    let qualificationListItem = document.createElement('li');
+    qualificationListItem.textContent = element;
+    qualificationsList.appendChild(qualificationListItem);
+}
+authorsInfo.appendChild(qualificationsList);
+
+authorsInfo.style.display = 'flex';
+authorsInfo.style.justifyContent = 'space-between'
+secondSection.appendChild(authorsInfo);
+
+let thirdSection = document.createElement('section');
+thirdSection.style.width = '80%';
+thirdSection.style.marginLeft = '100px'
+let keywordsSection = document.createElement('section');
+let keywords = document.createElement('h3');
+keywords.textContent = 'Keywords';
+keywords.style.textAlign = 'left';
+thirdSection.appendChild(keywords);
+thirdSection.appendChild(keywordsSection);
+keywordsSection.style.display = 'flex';
+keywordsSection.style.flexWrap = 'wrap';
+keywordsSection.style.justifyContent = 'space-between';
+let colors = ['coral', 'green', 'red', 'aqua', 'brown', 'grey', 'beige', 'cyan', 'purple', 'pink', 'teal', 'olive', 'indigo', 'orange', 'cadetblue', 'lavender', 'khaki', 'ivory', 'tan', 'aliceblue', 'cornsilk', 'lemonchiffon',
+'lime', 'lightblue', 'palegreen', 'sienna', 'thistle', 'tomato', 'seagreen', 'salmon', 'rosybrown', 'plum', 'olivedrab', 'burlywood', 'gold'];
+for (let index = 0; index < asabenehChallenges2020.keywords.length; index++) {
+    const element = asabenehChallenges2020.keywords[index];
+    let keyWord = document.createElement('p');
+    keyWord.textContent = "#" + element;
+    keyWord.style.fontSize = '9pt';
+    keyWord.style.margin = '3px';   
+    keyWord.style.backgroundColor = colors[index];
+    keyWord.style.borderRadius = '25px';
+    keyWord.style.padding = '5px';
+    keyWord.style.fontStyle = 'italic';
+    keyWord.style.paddingRight = '17px';
+    keywordsSection.appendChild(keyWord);
+}
+outerDiv.appendChild(thirdSection);
